28d386fc15ccdfb83851a1381b253a81c06defe1
Nikunj Vadher <nikunjjv27@gmail.com>
2022-12-23 19:27:55 +0530

    adding day-1 folder

---
+++ day-1/index.js
@@ -0,0 +1,58 @@
+
+
+// 1 Write a single line comment which says, comments can make code readable
+// comment can make code reliable
+
+// 2 Write another single comment which says, Welcome to 30DaysOfJavaScript
+// Welcome to 30DaysOfJavaScript
+
+// 3 Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/* comments can make code readable, 
+ easy to reuse and informative*/
+
+// 4 Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+let name = "Nikunj"
+let isMale = true
+let haveFrds;
+let haveABike = null;
+console.log(name , isMale , haveFrds , haveABike);
+
+// 5 Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+console.log( typeof name ,typeof isMale ,typeof haveFrds ,typeof haveABike);
+
+
+// 6 Declare four variables without assigning values
+let varA;
+let varB;
+let varC;
+let varD;
+
+// 7 Declare four variables with assigned values
+let varW = 5;
+let varX = 5;
+let varY = 5;
+let varZ = 5;
+
+// 8 Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstName = 'Nikunj'
+let lastName = 'Vadher'
+let meritalStatus = false
+let country = "India"
+let age = 18
+
+console.log(firstName);
+console.log(lastName);
+console.log(meritalStatus);
+console.log(country);
+console.log(age);
+
+// 9 Declare variables to store your first name, last name, marital status, country and age in a single line
+console.log(firstName ,lastName ,  meritalStatus , country , age);
+
+// 10 Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+// I am 25 years old.
+// You are 30 years old.
+let myAge = 25;
+let yourAge =305;
+console.log(`I am ${myAge} years old.`);
+console.log(`You are${yourAge} years old.`);
