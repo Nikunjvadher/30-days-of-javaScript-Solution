911e1d4c91f2b4f636ba12394f15a155d4d04413
Nikunj Vadher <nikunjjv27@gmail.com>
2023-01-28 11:24:28 +0530

    day11 level2 completed

---
+++ day-11/level2.js
@@ -0,0 +1,57 @@
+
+const users = [
+    {
+      name: "Brook",
+      scores: 75,
+      skills: ["HTM", "CSS", "JS"],
+      age: 16,
+    },
+    {
+      name: "Alex",
+      scores: 80,
+      skills: ["HTM", "CSS", "JS"],
+      age: 18,
+    },
+    {
+      name: "David",
+      scores: 75,
+      skills: ["HTM", "CSS"],
+      age: 22,
+    },
+    {
+      name: "John",
+      scores: 85,
+      skills: ["HTML"],
+      age: 25,
+    },
+    {
+      name: "Sara",
+      scores: 95,
+      skills: ["HTM", "CSS", "JS"],
+      age: 26,
+    },
+    {
+      name: "Martha",
+      scores: 80,
+      skills: ["HTM", "CSS", "JS"],
+      age: 18,
+    },
+    {
+      name: "Thomas",
+      scores: 90,
+      skills: ["HTM", "CSS", "JS"],
+      age: 20,
+    },
+  ];
+
+// 1 Iterate through the users array and get all the keys of the object using destructuring
+for (const {name , age , skills , scores} of users) {
+  console.log(name , age , skills , scores);
+}
+
+// 2 Find the persons who have less than two skills
+for(const user of users){
+    if(user.skills.length < 2){
+        console.log(user);
+    }
+}
