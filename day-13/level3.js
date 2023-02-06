7ae775c96ef86b28d02b3c12e2cf428a48e1b7a9
Nikunj Vadher <nikunjjv27@gmail.com>
2023-02-06 20:08:11 +0530

    day 13 level3 completed

---
+++ day-13/level3.js
@@ -0,0 +1,9 @@
+// Exercises:Level 2
+// 1. 10 > 2 * 10 use console.assert()
+console.assert(10 > 2 * 10, "10 is note greater than 20 ");
+
+// 2. Write a warning message using console.warn()
+console.warn("This is warning");
+
+// 3. Write an error message using console.error()
+console.error("This is an error");
